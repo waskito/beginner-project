e40c87f791e65530c65ebdf5449a47a6b1b17b6b
Yanuar Waskito <yg.waskito@gmail.com>
2018-01-24 00:39:47 +0700

    Update to single container

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
-const proxyMiddleware = require('http-proxy-middleware');
+const translate = require('google-translate-api');
+const bodyParser = require('body-parser');
 
-// Proxy to API server
-var translatorContext = [
-    '/translate',
-  ];                     // requests with this path will be proxied
+app
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, 'build')))
+  .use(morgan('dev'));
 
-var dataRead;
-var translatorOptions = {
-        target: process.env.TRANSLATE_API, // target host
-        // changeOrigin: true,              // needed for virtual hosted sites
-        // hostRewrite: config.origin,
-        // autoRewrite: config.origin,
-        // protocolRewrite:
-    };
+app.post('/api/translate', function(req, res){
+  const options = {};
+  if (req.body.from)
+    options.from = req.body.from;
+  if (req.body.to)
+    options.to = req.body.to;
+  translate(req.body.text, options).then(result => {
+      res.json({
+        code: 200,
+        data: result
+      })
+  }).catch(err => {
+      res.json({
+        code: 400,
+        error: err
+      })
+  });
+})
 
-// create the proxy
-var proxy = proxyMiddleware(translatorContext, translatorOptions);
-
-app.use(proxy);
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
+app.get(/^\/(?!api).*/, function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+
+
 
 
 app.listen(process.env.PORT);
